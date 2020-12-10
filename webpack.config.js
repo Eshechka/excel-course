8eb2dced7a2d272acfbd7ebb27764a39c3d1dec9
Lidia <ElidiyaE@yandex.ru>
2020-12-10 14:28:47 +0300

    finish markup and css

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProd = process.env.NODE_ENV === 'production'
-const isDev = !isProd
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
 
-const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
 
 const JSloaders = () => {
   const loaders = [
-  {
-    loader: 'babel-loader',
-    options: {
-      presets: ['@babel/preset-env']
-    }
-  } ;
-
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      },
+    },
+  ];
   if (isDev) loaders.push('eslint-loader');
-]
-} 
+};
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
@@ -83,4 +83,4 @@ module.exports = {
       }
     ]
   }
-}
+};
